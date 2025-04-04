f7bb42fc1d71be18be05dde1e5f33c45c8358e31
海屿有燕 <zorua@vip.qq.com>
2025-04-04 15:30:55 +0800

    revert

--- LocalSettings.php
+++ LocalSettings.php
@@ -175,11 +175,7 @@ $wgUseInstantCommons = false;
 $wgPingback = false;
 
 # 站点语言代码及变体
-// 定义一个常量来保存语言代码
-define('MW_LANGUAGE_CODE', 'zh');
-
-// 使用常量来设置语言代码
-$wgLanguageCode = MW_LANGUAGE_CODE;
+$wgLanguageCode = "zh";
 $wgDefaultLanguageVariant = "zh-cn";
 $wgVariantArticlePath = "/$2/$1";
 # Time zone
