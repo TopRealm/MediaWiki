963792ccb68a072c40f73a43aca6783473b84cb5
海屿有燕 <zorua@vip.qq.com>
2025-08-05 11:54:24 +0800

    调整缓存设置：将主缓存类型更改为CACHE_ACCEL，会话缓存类型更改为CACHE_ACCEL

--- LocalSettings.php
+++ LocalSettings.php
@@ -118,10 +118,10 @@ $wgSharedTables[] = "actor";
 
 ## 缓存设置
 # 缓存类型
-$wgMainCacheType = CACHE_MEMCACHED;
+$wgMainCacheType = CACHE_ACCEL;
 $wgParserCacheType = CACHE_ACCEL; // optional
 $wgMessageCacheType = CACHE_ACCEL; // optional
-$wgSessionCacheType = CACHE_DB;
+$wgSessionCacheType = CACHE_ACCEL;
 $wgMemCachedPersistent = true;
 
 // Alpha: Redis
